8a536230522251e14c41e1196e18757bf7b87740
Charlie Gordon <205866015+Neon6105@users.noreply.github.com>
2025-04-03 15:37:53 -0400

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -3,7 +3,7 @@ from os.path import isfile, join  # basename, dirname
 
 plugins_dir = "plugins"  # or basename(dirname(__file__))
 # List of plugins to skip. __init__.py is always skipped.
-skip_plugins = ("__template", "outfile")
+skip_plugins = ("__template", "file")
 
 # Get *.py files except tuple(skip_plugins), and remove the .py extension.
 modules = [f[:-3] for f in listdir(plugins_dir)
