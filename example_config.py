63897e12cc0be5d9496cb35e2e621b77779839f3
Charlie Gordon <205866015+Neon6105@users.noreply.github.com>
2025-04-04 12:13:27 -0400

    Cleanup example_config.py
    Removed "localcache" from skip_plugins, deleted placeholder Zabbix API key

--- example_config.py
+++ example_config.py
@@ -2,12 +2,12 @@ from os import getenv
 import os.name
 
 _vars = {
-  "ZPG_SKIP_PLUGINS":  ("template", "localcache"),
+  "ZPG_SKIP_PLUGINS":  ("template", ),
 
   "FILE_CACHE":        "C:\\Temp\\Pushgateway" if os.name == "nt" else "/tmp/pushgateway",
 
   "ZABBIX_ADDRESS":    "127.0.0.1",
-  "ZABBIX_API_TOKEN":  "3a5ae07dcdc53afd8b4e2a521161a03f7353ac6f09c19def87505e21a69501a6",
+  "ZABBIX_API_TOKEN":  "",
   "ZABBIX_KEY_PREFIX": "",
   "ZABBIX_USE_HTTPS":  False,
 }
